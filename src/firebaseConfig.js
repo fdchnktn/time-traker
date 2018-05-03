93861196596a2d6253322c2d666fcea002c6aa26
Tanya Fedchenko <30293847+fdchnktn@users.noreply.github.com>
2018-05-03 16:06:54 +0300

    Delete firebaseConfig.js

--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig =  {
-  apiKey: "AIzaSyB93Zzr51WoRLQXdzkyqR1t7WyZtxJd7BU",
-  authDomain: "time-traker-41edf.firebaseapp.com",
-  databaseURL: "https://time-traker-41edf.firebaseio.com",
-  projectId: "time-traker-41edf",
-  storageBucket: "",
-  messagingSenderId: "1010378607721"
-};
